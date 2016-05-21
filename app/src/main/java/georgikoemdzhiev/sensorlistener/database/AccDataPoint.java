20cdcafc48d3d38e7aceba9594ec7c82c0f8686f
boggogo <koemdjiev@gmail.com>
2016-05-21 15:58:13 +0100

    added class for the datapoints

---
+++ app/src/main/java/georgikoemdzhiev/sensorlistener/database/AccDataPoint.java
@@ -0,0 +1,36 @@
+package georgikoemdzhiev.sensorlistener.database;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by koemdzhiev on 21/05/16.
+ */
+public class AccDataPoint extends RealmObject {
+    private double x;
+    private double y;
+    private double z;
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+}
